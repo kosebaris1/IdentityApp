986e76567feaefb3c5b0a320c76db225e037d3b4
kosebaris1 <kosebaris464@gmail.com>
2025-05-05 14:06:19 +0300

    role details process

--- IdentityApp/Controllers/RolesController.cs
+++ IdentityApp/Controllers/RolesController.cs
@@ -7,9 +7,13 @@ namespace IdentityApp.Controllers
     public class RolesController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
-        public RolesController(RoleManager<AppRole> roleManager)
+
+        private readonly UserManager<AppUser> _userManager;
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager; 
+            _userManager = userManager;
+
         }
         public IActionResult Index()
         {
@@ -42,6 +46,20 @@ namespace IdentityApp.Controllers
             }
 
             return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role =await _roleManager.FindByIdAsync(id);
+
+            if(role != null && role.Name != null)
+            {
+                ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                return View(role);
+
+            }
+
+            return RedirectToAction("Index");
         }
 
     }
